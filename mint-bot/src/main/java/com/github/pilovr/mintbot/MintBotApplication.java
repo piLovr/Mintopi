13743c780c477baeb41390bbdbd6684b6a81c4de
piLovr <janlen2109@gmail.com>
2025-08-12 23:03:19 +0200

    Initial commit of multi-module project

---
+++ mint-bot/src/main/java/com/github/pilovr/mintbot/MintBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.pilovr.mintbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MintBotApplication.class, args);
+    }
+
+}
---
+++ mintopi-core/src/main/java/com/github/pilovr/mintopicore/Command.java
@@ -0,0 +1,6 @@
+package com.github.pilovr.mintopicore;
+
+public abstract class Command {
+    public abstract String getName();
+    public abstract void run();
+}
